f0060f4fd8e730ded5edc1c3688582a22477f95e
masterUNG <phrombutr@gmail.com>
2019-01-16 14:16:20 +0700

    Get URL Success

--- UngService/MyConstant.swift
+++ UngService/MyConstant.swift
@@ -11,6 +11,12 @@ import Foundation
 class MyConstant {
     
     let urlAddUser: String = "https://www.androidthai.in.th/hit/addUserMaster.php?isAdd=true&Name="
+    let urlGetUser: String = "https://www.androidthai.in.th/hit/getUserWhereUserMaster.php?isAdd=true&User="
+    
+    func getUrlGetUser(user: String) -> String {
+        let result = "\(urlGetUser)\(user)"
+        return result
+    }
     
     func getUrlAddUser() -> String {
         return urlAddUser
--- UngService/ViewController.swift
+++ UngService/ViewController.swift
@@ -10,14 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
     
+//    Explicit
+    var user: String?
+    var password: String?
     
     
+    @IBOutlet weak var userTextField: UITextField!
     
-
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }   // Main Function
+    
+    func checkAuthen(user: String, password: String) -> Void {
+        
+        let myConstant = MyConstant()
+        
+        let urlString: String = myConstant.getUrlGetUser(user: user)
+        print("urlString ==> \(urlString)")
+        
+        
     }
+    
+    func myAlert(title: String, message: String) -> Void {
+        let objAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        objAlert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+            objAlert.dismiss(animated: true, completion: nil)
+        }))
+        present(objAlert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+        user = userTextField.text
+        password = passwordTextField.text
+        
+        if (user!.count == 0) || (password!.count == 0) {
+//            Have Space
+            print("Have Space")
+            myAlert(title: "Have Space", message: "Please Fill All Blank")
+        }else {
+//            No Space
+            print("No Space")
+            checkAuthen(user: user!, password: password!)
+        }
+        
+        
+    }
+    
+    
 
 
 }
