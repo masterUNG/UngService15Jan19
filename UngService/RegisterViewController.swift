6c2f2d12e27b1a399dc0a69325a82d161713d054
masterUNG <phrombutr@gmail.com>
2019-01-16 09:47:35 +0700

    Check Space Success

--- UngService/RegisterViewController.swift
+++ UngService/RegisterViewController.swift
@@ -9,22 +9,60 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+//    Explicit
+    var name: String?
+    var user: String?
+    var password: String?
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var userTextField: UITextField!
+        
+    @IBOutlet weak var passwordTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }   // Main Function
+    
+    
+    @IBAction func cloudUploadButton(_ sender: Any) {
+        
+//        Get Value From TextField
+        name = nameTextField.text
+        user = userTextField.text
+        password = passwordTextField.text
+        
+//        Call Show Log
+        showLog(title: "name", message: name!)
+        showLog(title: "user", message: user!)
+        showLog(title: "password", message: password!)
+        
+//        Call checkSpace
+        checkSpace(name: name!, user: user!, password: password!)
+        
+        
+    }   // cloudUpload
+    
+    func checkSpace(name: String, user: String, password: String) -> Void {
+        
+        if (name.count == 0) || (user.count == 0) || (password.count == 0) {
+//            Have Space
+            showLog(title: "Have Space", message: "")
+        }else {
+//            No Space
+            showLog(title: "No Space", message: "")
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func showLog(title: String, message: String) -> Void {
+        print("\(title) = \(message)")
     }
-    */
-
-}
+    
+}   // Main Class
