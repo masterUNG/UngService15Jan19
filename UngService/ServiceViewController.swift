1b29dd0ff37a1d7548cdffe56c942672d0b3c470
masterUNG <phrombutr@gmail.com>
2019-01-17 05:49:32 +0700

    Complete Success

--- UngService/ServiceViewController.swift
+++ UngService/ServiceViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ServiceViewController: UIViewController {
+class ServiceViewController: UIViewController, AVAudioPlayerDelegate{
+    
+    let notes = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    var audioPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,17 @@ class ServiceViewController: UIViewController {
     @IBAction func myButton(_ sender: UIButton) {
         
         print("Tag ==> \(sender.tag)")
+        let index = (sender.tag) - 1
+        
+        let soundURL = Bundle.main.url(forResource: notes[index], withExtension: "wav")
+        
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        } catch let error as NSError {
+            print(error)
+        }
+        audioPlayer!.play()
         
     }
     
